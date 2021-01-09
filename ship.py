fbb8e3247f34caea012e4a36299e542aa0490788
leslie-x <798562309@qq.com>
2021-01-09 12:02:13 +0800

    set speed

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
         #init location
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
         
         #load ship.bmp
         self.image = pygame.image.load('images/ship.bmp')
@@ -15,6 +16,10 @@ class Ship:
         #locate ship at the bottom of the screen
         self.rect.midbottom = self.screen_rect.midbottom
         
+        #let x be float because speed step is a float
+        #since rect.x is an int not a float so instead by .x
+        self.x = float(self.rect.x)
+        
         #MOVE FLAG
         self.moving_right = False
         self.moving_left = False
@@ -22,10 +27,14 @@ class Ship:
     
     def update(self):
         """move location according to move flag"""
+        #refresh the ship's x by ship_speed 
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        
+        #rect.x restores .x's int like 1.5+1.5=3 rect.x=3
+        self.rect.x = self.x
         
         
     def blitme(self):
